3a0a71868d93860d6aa8e0d729ed95673eb5f530
peric <drazen.peric@kiwi.com>
2020-08-24 13:44:56 +0200

    colon to equal sign

--- variables.tf
+++ variables.tf
@@ -135,6 +135,6 @@ locals {
   function_name     = var.function_name != "" ? var.function_name : "${var.sls_project_name}-${var.entry_point}"
   region_app_engine = var.region_app_engine != "" ? var.region_app_engine : var.region
   labels            = concat({
-    deployment-tool: "terraform"
+    deployment-tool = "terraform"
   }, var.labels)
 }

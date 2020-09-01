ff4bae17884fb2c0a53c190b6eb063030c00e1ca
peric <drazen.peric@kiwi.com>
2020-09-01 12:47:04 +0200

    output topic id instead of name

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "function_http_url" {
   value = join(" ", google_cloudfunctions_function.function_http.*.https_trigger_url)
 }
 
-output "scheduler_topic_name" {
-  value = join(" ", google_pubsub_topic.scheduler.*.name)
+output "scheduler_topic_id" {
+  value = join(" ", google_pubsub_topic.scheduler.*.id)
 }

409ec8f454d4e3f8d677db5713a7ef6e4e531fed
peric <drazen.peric@kiwi.com>
2020-09-01 12:19:28 +0200

    output scheduler topic name

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "function_http_url" {
   value = join(" ", google_cloudfunctions_function.function_http.*.https_trigger_url)
 }
+
+output "scheduler_topic_name" {
+  value = join(" ", google_pubsub_topic.scheduler.*.name)
+}
